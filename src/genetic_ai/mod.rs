6f61e3d77f5f341b668b917d4e42ff8dcf275c14
roberte777 <robewilkes@yahoo.com>
2022-11-02 22:23:01 -0500

    started on the tree type

--- src/genetic_ai/mod.rs
+++ src/genetic_ai/mod.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 use rand::{distributions::Standard, prelude::*};
 pub struct Agent {
     pub fitness: i32,
@@ -5,28 +7,126 @@ pub struct Agent {
 }
 
 impl Agent {
-    pub fn new(depth_limit: u32) -> Agent {
+    pub fn new(depth_limit: u32, method: Method) -> Agent {
         Agent {
             fitness: 0,
-            gene: Tree::new(depth_limit),
+            gene: Tree::new(depth_limit, method),
         }
     }
 }
 
-struct Tree {
+pub enum Method {
+    Grow,
+    Full,
+}
+
+pub struct Tree {
     root: Node,
 }
 impl Tree {
-    fn new(depth_limit: u32) -> Tree {
+    fn new(depth_limit: u32, method: Method) -> Tree {
+        let starting_node = match method {
+            Method::Grow => Tree::grow(depth_limit, 0, 0.5),
+            Method::Full => Tree::full(depth_limit, 0),
+        };
         Tree {
-            root: Node::new_internal(),
+            root: starting_node,
         }
     }
+    fn grow(depth_limit: u32, curr_level: u32, threshhold: f32) -> Node {
+        if curr_level == depth_limit {
+            return Node::new_leaf();
+        }
+        let mut rng = rand::thread_rng();
+        let rand_num: f32 = rng.gen();
+        if rand_num < threshhold {
+            let mut new_node = Node::new_internal();
+            new_node.left = Some(Box::new(Tree::grow(
+                depth_limit,
+                curr_level + 1,
+                threshhold,
+            )));
+            new_node.right = Some(Box::new(Tree::grow(
+                depth_limit,
+                curr_level + 1,
+                threshhold,
+            )));
+            return new_node;
+        } else {
+            return Node::new_leaf();
+        }
+    }
+    fn full(depth_limit: u32, curr_level: u32) -> Node {
+        if curr_level == 0 {
+            let mut new_node = Node::new_internal();
+            new_node.left = Some(Box::new(Tree::full(depth_limit, curr_level + 1)));
+            new_node.right = Some(Box::new(Tree::full(depth_limit, curr_level + 1)));
+            return new_node;
+        } else if curr_level == depth_limit {
+            return Node::new_leaf();
+        } else {
+            let mut new_node = Node::new_internal();
+            new_node.left = Some(Box::new(Tree::full(depth_limit, curr_level + 1)));
+            new_node.right = Some(Box::new(Tree::full(depth_limit, curr_level + 1)));
+            return new_node;
+        }
+    }
+    fn print(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.print_recurse(&self.root, 0, f)
+    }
+    fn print_recurse(
+        &self,
+        node: &Node,
+        depth: u32,
+        f: &mut std::fmt::Formatter<'_>,
+    ) -> std::fmt::Result {
+        for _ in 0..depth {
+            write!(f, "|")?;
+        }
+        write!(f, "{}\n", node)?;
+        if let Some(left) = &node.left {
+            self.print_recurse(&left, depth + 1, f)?;
+        }
+        if let Some(right) = &node.right {
+            self.print_recurse(&right, depth + 1, f)?;
+        }
+        Ok(())
+    }
 }
+
+impl Display for Tree {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        // use this to collapse the tree
+        // let mut level = 0;
+        // let mut stack = vec![];
+        // let mut curr = &self.root;
+        // loop {
+        //     if curr.left.is_some() {
+        //         stack.push(curr);
+        //         curr = curr.left.as_ref().unwrap();
+        //     } else {
+        //         write!(f, "{}\n", curr)?;
+        //         if curr.right.is_some() {
+        //             curr = curr.right.as_ref().unwrap();
+        //         } else {
+        //             if stack.is_empty() {
+        //                 break;
+        //             }
+        //             curr = stack.pop().unwrap();
+        //             write!(f, "{}\n", curr)?;
+        //             curr = curr.right.as_ref().unwrap();
+        //         }
+        //     }
+        // }
+        self.print(f)?;
+        Ok(())
+    }
+}
+
 pub struct Node {
-    node_type: NodeType,
-    left: Option<Box<Node>>,
-    right: Option<Box<Node>>,
+    pub node_type: NodeType,
+    pub left: Option<Box<Node>>,
+    pub right: Option<Box<Node>>,
 }
 impl Node {
     fn new(node_type: NodeType) -> Node {
@@ -53,7 +153,17 @@ impl Node {
         }
     }
 }
-enum InternalNode {
+impl Display for Node {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match &self.node_type {
+            NodeType::Leaf(leaf) => write!(f, "{:?}", leaf),
+            NodeType::Internal(internal) => write!(f, "{:?}", internal),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum InternalNode {
     Add,
     Sub,
     Mul,
@@ -70,7 +180,8 @@ impl Distribution<InternalNode> for Standard {
         }
     }
 }
-enum LeafNode {
+#[derive(Debug)]
+pub enum LeafNode {
     SnakeLength,
     SnakeDirection,
     AppleLocation,
@@ -85,7 +196,7 @@ impl Distribution<LeafNode> for Standard {
         }
     }
 }
-enum NodeType {
+pub enum NodeType {
     Internal(InternalNode),
     Leaf(LeafNode),
 }
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,28 @@
 use rand::prelude::*;
-use snake::gym::{game::Game, snake::Direction};
+use snake::{
+    genetic_ai::{Agent, Method},
+    gym::{game::Game, snake::Direction},
+};
 fn main() {
+    let agent = Agent::new(5, Method::Full);
+    println!("{}", agent.gene);
     let mut r = rand::thread_rng();
     let mut game = Game::new();
     let mut count = 0;
-    while !game.lost {
-        if count % 5 == 0 {
-            let idx = r.gen_range(0..4);
-            match idx {
-                0 => game.update_direction(Direction::Up),
-                1 => game.update_direction(Direction::Down),
-                2 => game.update_direction(Direction::Left),
-                3 => game.update_direction(Direction::Right),
-                _ => {}
-            }
-        }
-        game.display();
-        game.update();
-        count += 1;
-    }
+    // while !game.lost {
+    //     if count % 5 == 0 {
+    //         let idx = r.gen_range(0..4);
+    //         match idx {
+    //             0 => game.update_direction(Direction::Up),
+    //             1 => game.update_direction(Direction::Down),
+    //             2 => game.update_direction(Direction::Left),
+    //             3 => game.update_direction(Direction::Right),
+    //             _ => {}
+    //         }
+    //     }
+    //     game.display();
+    //     game.update();
+    //     count += 1;
+    // }
     println!("Game Over: {}", game.score);
 }
